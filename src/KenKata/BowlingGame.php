846f3b2f03bd88245231a8aaf61ff1da17e7008a
Ken Lin <twinkledj@Kens-MacBook-Pro.local>
2015-05-01 11:33:21 +0800

    Comment 3rd and redesign method.

--- src/KenKata/BowlingGame.php
+++ src/KenKata/BowlingGame.php
@@ -5,14 +5,19 @@ namespace KenKata;
 class BowlingGame
 {
     private $score = 0;
+    private $rolls = [];
 
     public function roll($pin)
     {
-        $this->score += $pin;
+        $this->rolls[] = $pin;
     }
 
     public function score()
     {
+        foreach ($this->rolls as $pin) {
+            $this->score += $pin;
+        }
+
         return $this->score;
     }
 }
