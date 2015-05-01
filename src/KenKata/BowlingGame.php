6e77247b2b346b64a7c0c1d180aa72dcea00ed03
Ken Lin <twinkledj@Kens-MacBook-Pro.local>
2015-05-01 11:43:14 +0800

    Finally passed 3rd test, ready to refactor.

--- src/KenKata/BowlingGame.php
+++ src/KenKata/BowlingGame.php
@@ -14,12 +14,14 @@ class BowlingGame
 
     public function score()
     {
+        $i = 0;
         for ($frame = 0; $frame < 10; $frame++) {
-            if ($this->rolls[$frame] + $this->rolls[$frame+1] == 10) {
-                $this->score += 10 + $this->rolls[$frame+2];
+            if ($this->rolls[$i] + $this->rolls[$i+1] == 10) {
+                $this->score += 10 + $this->rolls[$i+2];
             } else {
-                $this->score += $this->rolls[$frame] + $this->rolls[$frame+1];
+                $this->score += $this->rolls[$i] + $this->rolls[$i+1];
             }
+            $i += 2;
         }
 
         return $this->score;
